a16c8d9b74b7ccfb271b023688b2a021bddcd355
blardtony <tonyblard55@gmail.com>
2024-11-29 16:55:39 +0100

    :sparkles: Create header

---
+++ src/components/layouts/MainLayout/Header/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<header>
+			<div className="container mx-auto px-4 md:px-0 h-16 flex items-center justify-between">
+				<div className="font-bold text-xl">Header</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header/Header';
 
 const MainLayout: React.FC = () => {
 	return (
 		<>
+			<Header />
 			<div className="container mx-auto px-4 md:px-0 h-dvh">
 				<Outlet />
 			</div>
