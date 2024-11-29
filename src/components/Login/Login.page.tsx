187145d1adde834ecc1306559c602c908576e7e9
blardtony <tonyblard55@gmail.com>
2024-11-29 17:19:26 +0100

    :sparkles: Add background image in login page

--- src/components/Login/Login.page.tsx
+++ src/components/Login/Login.page.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+import loginImage from '../../assets/images/background_login.png';
 const LoginPage = () => {
+	useEffect(() => {
+		document.body.style.backgroundImage = `url(${loginImage})`;
+		return () => {
+			document.body.style.backgroundImage = '';
+		};
+	});
 	return (
 		<div>
 			<h2>Login</h2>
