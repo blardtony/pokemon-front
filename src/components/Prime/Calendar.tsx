170c16f00d8a25a958e73cd7494d0474e6043a07
blardtony <tonyblard55@gmail.com>
2024-12-05 17:03:26 +0100

    :sparkles: Theming calendar from primereact

---
+++ src/components/Prime/Calendar.tsx
@@ -0,0 +1,15 @@
+import { PrimeReactProvider } from 'primereact/api';
+import { Calendar as PrimeCalendar, type CalendarProps } from 'primereact/calendar';
+import { Theming } from './Theming';
+
+export const Calendar = (props: CalendarProps) => {
+	return <Component {...props} />;
+};
+
+export const Component = (props: CalendarProps) => {
+	return (
+		<PrimeReactProvider value={{ pt: Theming }}>
+			<PrimeCalendar {...props} />
+		</PrimeReactProvider>
+	);
+};
